613ec0a38a7d825213e265cb66e406a7ebaf897a
jc374181 <keegan.bentley@my.jcu.edu.au>
2017-03-13 12:29:23 +1000

    Add oddName.py formatting

--- prac 4/oddName.py
+++ prac 4/oddName.py
@@ -1,14 +1,24 @@
 
 ''' Keegan '''
 
-name = input("Enter your name:")
-while name == "":
-    print("You must enter your name.")
+
+def main():
+    name = get_name()
+    letter_count = 1
+    try:
+        for i in range(len(name)):
+            print(name[letter_count])
+            letter_count += 2
+    except IndexError:
+        print("End of name (:")
+
+
+def get_name():
     name = input("Enter your name:")
-letter_count = 1
-try:
-    for i in range(len(name)):
-        print(name[letter_count])
-        letter_count += 2
-except IndexError:
-    print("End of name (:")
+    while name == "":
+        print("You must enter your name.")
+        name = input("Enter your name:")
+    return name
+
+
+main()
