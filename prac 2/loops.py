ca1dccebe7016e20be7367a5866b94c256f61422
jc374181 <keegan.bentley@my.jcu.edu.au>
2017-03-13 11:33:21 +1000

    Add Initial commit

---
+++ prac 2/loops.py
@@ -0,0 +1,11 @@
+for i in range(1, 21, 2):
+    print(i, end=' ')
+print()
+for i in range(0,101, 10):
+    print(i, end=' ')
+print()
+number = 20
+for i in range(20):
+    print(number, end=' ')
+    number = number - 1
+print()
