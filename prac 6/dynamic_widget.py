be67c19b443011625758b3f7f97504212042d69d
KeeganBentley <keegan.bentley@my.jcu.edu.au>
2017-04-10 11:36:44 +1000

    Finish dynamic widget exercise, Add some class rules messing around

--- prac 6/dynamic_widget.py
+++ prac 6/dynamic_widget.py
@@ -3,5 +3,25 @@
 Kivy program to practice dynamically creating widgets
 started 10/4/17
 """
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
 
 class DynamicWidgetsApp(App):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.list = ['label 1', 'label 2', 'label 3', 'label 4']
+
+    def build(self):
+        self.title = 'List of Labels'
+        self.root = Builder.load_file('dynamic_widget.kv')
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        for value in self.list:
+            temp_label = Label(text=value)
+            self.root.ids.dynamicBox.add_widget(temp_label)
+
+DynamicWidgetsApp().run()
